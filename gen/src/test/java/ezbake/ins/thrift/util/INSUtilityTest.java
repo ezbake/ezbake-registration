ce8ab27861a111af27c786830dd70d2539c9dd57
bhammen <bhammen@42six.com>
2014-10-12 16:13:04 -0400

    Add license

--- gen/src/test/java/ezbake/ins/thrift/util/INSUtilityTest.java
+++ gen/src/test/java/ezbake/ins/thrift/util/INSUtilityTest.java
@@ -8,6 +8,20 @@ public class INSUtilityTest {
     @Test
     public void testSplitURIPrefix() throws Exception {
         String prefix = INSUtility.getUriPrefix("NEWS://myFeed/myUniqueId/can_have/slashes.xml");
+/*   Copyright (C) 2013-2014 Computer Sciences Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. */
+
         assertEquals("NEWS://myFeed/", prefix);
     }
 
--- impl/src/test/java/ezbake/ins/thrift/impl/InternalNameServiceTest.java
+++ impl/src/test/java/ezbake/ins/thrift/impl/InternalNameServiceTest.java
@@ -354,6 +354,20 @@ public class InternalNameServiceTest {
 		    // searched uri
 		    Map.Entry<String, WebApplicationLink> urnEntry = expected.getUrnMap().entrySet().iterator().next();
 		    String uri = urnEntry.getKey() + "/myUniqueId/12323213.xml";
+/*   Copyright (C) 2013-2014 Computer Sciences Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. */
+
 		
 		    // test - expected one application with same uri
 		    Set<WebApplicationLink> actual = client.getWebAppsForUri("NEWS://CNN");
